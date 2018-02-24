74633778a206b7566d8870d85ac8da5aae83acb7
bishesh <bisheshkh@gmail.com>
2018-02-24 09:39:26 +0000

    Nikola auto commit.
    Nikola version: 7.8.4

---
+++ bibtex/auto/my_publications.el
@@ -0,0 +1,14 @@
+(TeX-add-style-hook
+ "my_publications"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "Hou_2018"
+    "Khanal_2017"
+    "Khanal_2016_Thesis"
+    "Khanal_2016"
+    "Khanal_2015"
+    "Khanal_2014"
+    "Khanal_2012"
+    "Khanal_2011"))
+ :bibtex)
+
